cf111ba448ba9ccebb3e3583c6a9a36262f837fb
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-03-29 18:37:18 +0200

    some simple jquery

--- content-slider/js/script.js
+++ content-slider/js/script.js
@@ -1,3 +1,50 @@
-/**
- * Created by Andrzej on 2017-03-29.
- */
+$(document).ready(function () {
+    // set options
+    var speed = 500;                // fade speed
+    var autoswitch = true;          // auto slider options
+    var autoswitch_speed = 4000;    // auto slider speed
+
+    // Add initial active class
+    $('.slide').first().addClass('active');
+
+    // Hide all slides
+    $('.slide').hide();
+
+    // show first slide
+    $('.active').show();
+
+    // Next handler
+    $('#next').on('click', nextSlide);
+
+    $('#prev').on('click', prevSlide);
+
+    if(autoswitch == true){
+        setInterval(nextSlide, autoswitch_speed);
+    }
+
+    // Switch to the next slide function
+    function nextSlide(){
+        $('.active').removeClass('active').addClass('oldActive');
+        if($('.oldActive').is(':last-child')){
+            $('.slide').first().addClass('active');
+        } else {
+            $('.oldActive').next().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+
+    // Switch to previous slide
+    function prevSlide(){
+        $('.active').removeClass('active').addClass('oldActive');
+        if($('.oldActive').is(':first-child')){
+            $('.slide').last().addClass('active');
+        } else {
+            $('.oldActive').prev().addClass('active');
+        }
+        $('.oldActive').removeClass('oldActive');
+        $('.slide').fadeOut(speed);
+        $('.active').fadeIn(speed);
+    }
+});
